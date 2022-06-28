56795bd1ae0250cbcdb60d91f70f90844e336dd5
Hannah Veitch <hannah.j.veitch@gmail.com>
2022-06-28 15:31:39 +1000

    cli.py implemented - -h works with descriptions

--- pomegranate/cli.py
+++ pomegranate/cli.py
@@ -1,1 +1,15 @@
 """Command line interface for pomegranate."""
+
+import argparse
+
+parser = argparse.ArgumentParser(description="""POMEGRANATE
+PhOsphosite Motif Explorer - GRAph Network Abstraction Through Embeddings""")
+
+parser.add_argument('database', metavar='database', type=str, nargs=1,
+                    help='the database to search for the protein from (AlphaFold or PDB)')
+parser.add_argument('PDB_filename', metavar='PDB_filename', type=str,       
+                    nargs=1, help='the name of a local PDB file you wish to use')
+parser.add_argument('radius', metavar='radius', type=float, nargs=1,
+                    help='the radius to be examined around the phosphorylation site')
+
+args = parser.parse_args()

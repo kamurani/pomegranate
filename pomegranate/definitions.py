9b66f929e021edeb0ff3afa85b4dafd5fb4693df
Cam <cameronmcmenamie@gmail.com>
2022-07-26 22:08:34 +1000

    change embedding csv filename

--- pomegranate/definitions.py
+++ pomegranate/definitions.py
@@ -4,3 +4,7 @@ import os
 source_dir = os.path.dirname(os.path.abspath(__file__))
 ROOT_DIR = os.path.dirname(source_dir)
 STRUCTURE_PATH = os.path.join(ROOT_DIR, 'structures')
+STRUCTURE_HUMAN_PATH = os.path.join(STRUCTURE_PATH, 'human')
+
+EMBEDDINGS_FILENAME = "embeddings_output.csv" #'embeddings.csv'
+EMBEDDINGS_PATH = os.path.join(os.path.join(ROOT_DIR, 'embeddings'), EMBEDDINGS_FILENAME)
